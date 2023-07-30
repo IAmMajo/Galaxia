fdd57f8d913cdc7840f5bab49e024470058d4106
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2023-07-30 21:25:20 +0200

    added Mockdata

---
+++ lib/models/tagcloud_model.dart
@@ -0,0 +1,45 @@
+class TagCloudModel {
+  String name;
+
+  TagCloudModel({
+    required this.name,
+
+  });
+
+  static List<TagCloudModel> getTagcloud() {
+    List<TagCloudModel> tagcloud = [];
+
+    tagcloud.add(
+      TagCloudModel(
+          name: 'Marvel',
+    ));
+
+    tagcloud.add(
+      TagCloudModel(
+        name: 'DC',
+    ));
+
+    tagcloud.add(
+      TagCloudModel(
+          name: 'Action',
+    ));
+     tagcloud.add(
+      TagCloudModel(
+          name: 'Commedy',
+    ));
+    tagcloud.add(
+      TagCloudModel(
+          name: 'Thriller',
+    ));
+    tagcloud.add(
+      TagCloudModel(
+          name: 'Disney',
+    ));
+    tagcloud.add(
+      TagCloudModel(
+          name: 'Pixar',
+    ));
+
+    return tagcloud;
+  }
+}
