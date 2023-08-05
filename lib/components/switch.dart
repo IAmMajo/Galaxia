487839bd462b6cd8899de28cab0d4f31af7d64cf
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2023-08-05 16:05:18 +0200

    first idea of Settingsscreen

---
+++ lib/components/switch.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [Switch].
+
+void main() => runApp(const SwitchApp());
+
+class SwitchApp extends StatelessWidget {
+  const SwitchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchExample();
+
+  }
+}
+
+class SwitchExample extends StatefulWidget {
+  const SwitchExample({super.key});
+
+  @override
+  State<SwitchExample> createState() => _SwitchExampleState();
+}
+
+class _SwitchExampleState extends State<SwitchExample> {
+  bool light0 = false;
+
+  final MaterialStateProperty<Icon?> thumbIcon =
+      MaterialStateProperty.resolveWith<Icon?>(
+    (Set<MaterialState> states) {
+      if (states.contains(MaterialState.selected)) {
+        return const Icon(Icons.check);
+      }
+      return const Icon(Icons.close);
+    },
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Switch(
+          value: light0,
+          onChanged: (bool value) {
+            setState(() {
+              light0 = value;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,11 +1,116 @@
+import 'dart:ui';
+import 'package:galaxia/models/highlight_model.dart';
 import 'package:flutter/material.dart';
 import 'package:galaxia/components/logout.dart';
+import 'package:galaxia/components/switch.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Logout();
+  List<HighlightModel> highlight = [];
+
+    void getHighlight() {
+      highlight = HighlightModel.getHighlight();
+    }
+
+getHighlight();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(highlight[0].image),
+            fit: BoxFit.cover,
+          )
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          child: Container(
+            color: Colors.black.withOpacity(0.7),
+            child: ListView(
+              children: [
+               _logOutSec(),
+               const SizedBox( height: 15,),
+               Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Column(
+                children: [
+                  Row(
+                    children: [
+                     Padding(
+                       padding: const EdgeInsets.only(right: 8.0),
+                       child: const Text('Altersfreigabe',
+                        textAlign: TextAlign.start,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                        ),
+                                         ),
+                     ),
+                      const Expanded(
+                                      flex: 1,
+                                      child: Divider(
+                                        color: Colors.white,
+                                        thickness: 2,
+                                      ),
+                                    ),
+                    ]
+                  ),
+                  Row(
+                    children: [
+                      SwitchExample(),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Text('Inhalte mit FSK 18 ausblenden'),
+                      )
+                    ],
+                  )
+                ],
+               ),
+             ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding _logOutSec() {
+    return Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Column(
+                children: [
+                  Row(
+                    children: [
+                     Padding(
+                       padding: const EdgeInsets.only(right: 8.0),
+                       child: const Text('Log-Out',
+                        textAlign: TextAlign.start,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                        ),
+                                         ),
+                     ),
+                      const Expanded(
+                                      flex: 1,
+                                      child: Divider(
+                                        color: Colors.white,
+                                        thickness: 2,
+                                      ),
+                                    ),
+                    ]
+                  ),
+                  Logout()
+                ],
+               ),
+             );
   }
 }
+
+
+//Logout()
