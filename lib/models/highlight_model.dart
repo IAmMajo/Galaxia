342602d96c5e5537be559b38c8be8699eec78928
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2023-08-07 10:51:11 +0200

    Try to fix directing

--- lib/models/highlight_model.dart
+++ lib/models/highlight_model.dart
@@ -1,34 +1,56 @@
 class HighlightModel {
   String name;
   String logo;
+  String laenge;
   String genre;
   String image;
+  String fsk;
+  String des;
+  String actor;
   String platform;
+  String url;
 
   HighlightModel({
-    required this.name,
-    required this.logo,
-    required this.genre,
-    required this.image,
-    required this.platform,
+      required this.name,
+      required this.logo,
+      required this.laenge,
+      required this.genre,
+      required this.image,
+      required this.des,
+      required this.fsk,
+      required this.actor,
+      required this.platform,
+      required this.url
   });
 
   static List<HighlightModel> getHighlight() {
     List<HighlightModel> highlight = [];
 
     highlight.add(HighlightModel(
-        name: 'Supergirl',
-        logo: "assets/logos/SupergirlLogo.webp",
-        genre: 'DC Comics',
-        image: "assets/series/Supergirl.webp",
-        platform: 'Netflix'));
+         name: 'Titans',
+          laenge: '4 Staffeln / 37 Folgen',
+          logo: 'assets/logos/TitansLogo.png',
+          genre: 'DC',
+          fsk: 'assets/FSK16',
+          actor: 'Brenton Thwaites, Teagan Croft, Anna Diop',
+          des:
+              '"Titans" ist eine actiongeladene Superhelden-Serie, die auf dem beliebten DC-Comic-Team "Teen Titans" basiert. Die Handlung folgt einer Gruppe junger Superhelden, angeführt von Robin (Dick Grayson), die sich zusammenschließen, um das Böse zu bekämpfen und die Welt zu retten. Jedes Mitglied hat eine einzigartige Fähigkeit und Hintergrundgeschichte, die im Verlauf der Serie erkundet werden. "Titans" bietet spektakuläre Action-Szenen, komplexe Charakterentwicklungen und eine packende Handlung voller überraschender Wendungen.',
+          image: "assets/series/Titans.jpg",
+          platform: "assets/NetflixLogo.webp",
+          url: "https://www.netflix.com/search?q=Titans&jbv=80218200"));
 
     highlight.add(HighlightModel(
-        name: 'Game of Thrones',
-        logo: "assets/logos/GameofThronesLogo.png",
-        genre: 'Middleage | Fantasy',
-        image: "assets/series/GameofThrones.jpg",
-        platform: 'Sky'));
+          name: 'Supergirl',
+          laenge: '6 Staffeln / 120 Folgen',
+          logo: 'assets/logos/SupergirlLogo.webp',
+          genre: 'DC',
+          fsk: 'assets/FSK16',
+          actor: 'Melissa Benoist, Chyler Leigh, Katie McGrath',
+          des:
+              '"Supergirl" ist eine mitreißende Superhelden-Serie, die die Geschichte von Kara Zor-El erzählt, der Cousine von Superman. Nachdem sie jahrelang ihre Kräfte verborgen hat, entscheidet sich Kara, ihre Superkräfte einzusetzen, um die Menschen zu beschützen und als Supergirl die Welt zu retten. Die Serie verfolgt ihre Abenteuer und Herausforderungen, während sie gegen mächtige Bösewichte und persönliche Konflikte kämpft. "Supergirl" bietet spannende Action, emotionale Entwicklungen und eine inspirierende Botschaft über Mut, Selbstfindung und Heldentum.',
+          image: "assets/series/Supergirl.webp",
+          platform: "assets/NetflixLogo.webp",
+          url: "https://www.netflix.com/search?q=Supergirl&jbv=80065386"));
 
     return highlight;
   }
